22e0fd159ef141c2fd383ef1108531bfc7facfde
kspviswa <7476271+kspviswa@users.noreply.github.com>
2024-02-01 22:43:18 -0600

    refactored UI

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ def main(page: ft.Page) -> None:
     page.theme_mode = 'light'
     page.scroll = ft.ScrollMode.ADAPTIVE
     page.bgcolor = '#C7F9D6'
-    page.window_max_height = 600
-    page.window_max_width= 800
-    page.window_min_height = 600
-    page.window_min_width= 800
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    #page.window_resizable = True
+
+    #page.window_min_height = 600
+    #page.window_min_width= 800
+    #page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    #page.horizontal_alignment_alignment = ft.CrossAxisAlignment.CENTER
     page.theme = ft.theme.Theme(font_family="CabinSketch-Regular")
     page.fonts = {
     "Roboto Mono": "RobotoMono-VariableFont_wght.ttf",
@@ -22,20 +23,27 @@ def main(page: ft.Page) -> None:
     "CabinSketch-Bold" : "fonts/CabinSketch-Bold.ttf",
     "CabinSketch-Regular" : "fonts/CabinSketch-Regular.ttf"
     }
-    page.appbar = ft.AppBar(
-        title=ft.Text("pyOllama"),
-        center_title=True,
-        bgcolor='#C7F9D6',
-    )
+    # page.appbar = ft.AppBar(
+    #     title=ft.Text("pyOllama"),
+    #     center_title=True,
+    #     bgcolor='#C7F9D6',
+    # )
+    banner_image = ft.Image(src=f"logos/pyollama_1.png",
+                      width=125,
+                      height=125,
+                      fit=ft.ImageFit.CONTAIN,
+                      )
+    banner_text = ft.Text(value='pyOllaMx', style=ft.TextStyle(font_family='CabinSketch-Bold'), size=30)
     chat_messages = ft.Column(
                               alignment=ft.MainAxisAlignment.CENTER,
-                              tight=True,
-                              wrap=True
+                              scroll=ft.ScrollMode.ADAPTIVE,
+                              height=150,
+                              width=650
                               )
+    #chat_messages = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
     user_text = ft.Text(value='Enter your prompt', style=ft.TextStyle(font_family='CabinSketch-Bold'))
     user_text_field = ft.TextField(multiline=True,
-                                   width=200,
-                                   expand=True)
+                                   width=675)
     send_button = ft.ElevatedButton(text="Send")
     pr = ft.ProgressRing(width=16, height=16, stroke_width=2)
     pr.value = 0
@@ -50,10 +58,12 @@ def main(page: ft.Page) -> None:
     )
     select_mlX_models = ft.Switch(label='Load 🖥️ mlX models from HF',
                                   value=False,
-                                  adaptive=True)
+                                  adaptive=True,
+                                  label_position=ft.LabelPosition.LEFT)
     temp_label = ft.Text(value='Temperature')
     temp_slider = ft.CupertinoSlider(min=0.0, max=1.0, divisions=10, value=0.3)
     temp_value_label = ft.Text(value=temp_slider.value)
+    model_help_text = ft.Text('^Ollama models are loaded by default', style=ft.TextStyle(size=10), text_align=ft.TextAlign.LEFT)
 
     def updateChat(message: Message, ai_response: bool = False):
         if ai_response:
@@ -66,8 +76,7 @@ def main(page: ft.Page) -> None:
                     ft.Container(content=ft.Markdown(value=message.text, extension_set="gitHubWeb", code_theme='obsidian', code_style=ft.TextStyle(font_family='Roboto Mono'),selectable=True),
                                  width=550)
                 ],
-                
-                width=500
+                width=500, vertical_alignment=ft.CrossAxisAlignment.START
                 )
             )
         else:
@@ -87,7 +96,7 @@ def main(page: ft.Page) -> None:
 
 
     def show_spinning():
-        pr_ph.value='Thinking... 💭💭💭'
+        pr_ph.value='Working...🏃🏻‍♂️⏳'
         pr.value = None
         user_text_field.disabled = True
         page.update()
@@ -132,31 +141,40 @@ def main(page: ft.Page) -> None:
     model_dropdown.on_change = enableSend
     select_mlX_models.on_change = swapModels
     temp_slider.on_change = displayTemp
-    page.add(ft.Row([ft.Image(src=f"logos/pyollama_1.png",
-                      width=150,
-                      height=150,
-                      fit=ft.ImageFit.CONTAIN,
-                      )],
-                      alignment=ft.MainAxisAlignment.CENTER))
-    page.add(chat_messages)
-    page.add(ft.Row([
-        user_text,
-        ft.Row([temp_slider, temp_label, temp_value_label]
-        ),
-    ]))
-    page.add(ft.Row([
-        user_text_field,
-        send_button,
-    ]))
-    page.add(ft.Row([
-        model_dropdown,
+
+    top_banner_view = ft.Row([ft.Column([banner_image, banner_text], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER)], alignment=ft.MainAxisAlignment.CENTER)
+
+    temp_control_view = ft.Column([
+        temp_slider,
+        ft.Row([
+            temp_label,
+            temp_value_label
+        ])
+    ])
+
+    model_control_view = ft.Row([
+        ft.Column([model_dropdown]),
+        ft.Column([select_mlX_models, model_help_text], horizontal_alignment=ft.CrossAxisAlignment.START)
+    ], alignment=ft.MainAxisAlignment.CENTER, spacing=20)
+
+    controls_view = ft.Row([
+        temp_control_view,
+        model_control_view
+    ], alignment=ft.MainAxisAlignment.CENTER, spacing=20)
+
+    user_input_view = ft.Row([
         ft.Column([
-            select_mlX_models,
-            ft.Text('^Ollama models are loaded by default', style=ft.TextStyle(size=14), text_align=ft.TextAlign.RIGHT),
+            user_text,
+            user_text_field
         ]),
-    ],
-    ))
-    page.add(ft.Column([pr,pr_ph]))
+        send_button
+    ], vertical_alignment=ft.CrossAxisAlignment.CENTER)
+    
+    page.add(top_banner_view)
+    page.add(controls_view)
+    page.add(chat_messages)
+    page.add(ft.Row([pr,pr_ph]))
+    page.add(user_input_view)
     print(f'Window  width {page.width}')
     print(f'Window  height {page.height}')
 
